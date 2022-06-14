0608d233446765df3875c67478383d9ca25d3ca9
nazo <nazo@nazos-mbp.home>
2022-06-14 20:01:06 +0100

    Add Basic Encyclopedia

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+import random
+
+pokemon_id = random.choice(range(1, 145))
+
+response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}")
+# Converts from JavaScript Object Notation into Python lists and dictionaries
+pokemon_data = response.json()
+
+
+# Converts from JavaScript Object Notation into Python lists and dictionaries
+pokemon_data = response.json()
+
+print(f"You got pokemon number {pokemon_data['id']}, {pokemon_data['name'].capitalize()}!")
+print(f"Height: {pokemon_data['height']} • Weight: {pokemon_data['weight']}")
+
+pokemon_types = []
+for type_data in pokemon_data['types']:
+    pokemon_types.append(type_data['type']['name'])
+
+print(f"Type(s): {' and '.join(pokemon_types)}")
