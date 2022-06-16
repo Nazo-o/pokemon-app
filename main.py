cc44f990c334e152006e6d3b2cfc598616e59c02
nazo <nazo@nazos-mbp.home>
2022-06-16 19:07:04 +0100

    add multiple pokemon

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 import requests
 import random
 
-pokemon_id = random.choice(range(1, 145))
+pokemon_ids = random.sample(range(1, 145),6)
 
-response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}")
+for pokemon_id in pokemon_ids:
+    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}")
 # Converts from JavaScript Object Notation into Python lists and dictionaries
-pokemon_data = response.json()
+    pokemon_data = response.json()
 
 
 # Converts from JavaScript Object Notation into Python lists and dictionaries
-pokemon_data = response.json()
+    pokemon_data = response.json()
 
-print(f"You got pokemon number {pokemon_data['id']}, {pokemon_data['name'].capitalize()}!")
-print(f"Height: {pokemon_data['height']} • Weight: {pokemon_data['weight']}")
+    print(f"You got pokemon number {pokemon_data['id']}, {pokemon_data['name'].capitalize()}!")
+    print(f"Height: {pokemon_data['height']} • Weight: {pokemon_data['weight']}")
 
-pokemon_types = []
-for type_data in pokemon_data['types']:
-    pokemon_types.append(type_data['type']['name'])
+    pokemon_types = []
+    for type_data in pokemon_data['types']:
+        pokemon_types.append(type_data['type']['name'])
 
-print(f"Type(s): {' and '.join(pokemon_types)}")
+    print(f"Type(s): {' and '.join(pokemon_types)}")
+
+    print()
